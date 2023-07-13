5e72989fb4a0125580a9c5e845190499e382f110
luismartinez375 <luisleonardomartinez375@live.com>
2023-07-13 15:37:01 -0500

    feat: added functionality

--- src/app/components/listItem/listItem.tsx
+++ src/app/components/listItem/listItem.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import users from '../../../../db';
 import deleteIcon from '../../../../public/delete.svg';
 import edit from '../../../../public/edit.svg';
 import { User } from '../../../../types';
@@ -8,27 +7,41 @@ export interface IListItem {
 }
 
 export default function ListItem(user: User) {
-  const { id, Name, PhoneNumber, Email, EmergenyContactname, EmergencyContact } =
-    user;
+  const { id, Name } = user;
+  const remove = (id: number) => {
+    let dataList: User[] = JSON.parse(localStorage.getItem('users') || '[]');
+    for (let i = 0; i < dataList.length; i++) {
+      if (dataList[i].id === id) {
+        dataList.splice(i, 1);
+        localStorage.setItem('users', JSON.stringify(dataList));
+      }
+    }
+  };
+  const editUser = (id: number) => {
+    localStorage.setItem('editId', id.toString());
+    window.location.href = '/editUser';
+  };
   return (
     <>
-      <main className="flex flex-col sm:bg-bgWeb sm:w-4/5 sm:self-center">
-        <div className=" font-montserrat text-white flex flex-row justify-around">
-          <h1>{Name}</h1>
-          <div>
-            <button className=" pr-4 w-auto h-auto">
+      <main className="flex flex-col items-center sm:bg-bgWeb w-4/5 max-sm:w-full">
+        <div className=" font-montserrat text-white flex flex-row justify-between items-center w-1/2">
+          <h1 className="">{Name}</h1>
+          <div className=" w-20">
+            <button
+              className=" pr-4 w-auto h-auto"
+              onClick={() => {
+                editUser(id);
+              }}
+            >
               <Image src={edit} alt={'edit icon'}></Image>
             </button>
 
-            <button className=" w-auto h-auto" onClick={() => {
-              const index = users.findIndex((user) => user.id === id);
-              users.splice(index, 1);
-            }}>
+            <button className=" w-auto h-auto" onClick={() => remove(id)}>
               <Image src={deleteIcon} alt={'delete icon'}></Image>
             </button>
           </div>
         </div>
-        <div className=" max-md:w-5/6 h-px m-3 bg-slate-800 self-center sm:w-4/5"></div>
+        <div className=" max-md:w-5/6 h-px m-3 bg-slate-700 self-center sm:w-7/12"></div>
       </main>
     </>
   );
