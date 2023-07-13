eba635f3479b637e8ba2fafae9462131a6400ded
bear <manbearpig375@gmail.com>
2023-07-12 23:56:43 -0500

    feat: added submit functionality

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import back from '../../../public/back.svg';
 import UserForm from '../components/signInForm/signInForm';
-export default function page() {
+export default function Page() {
   const router = useRouter();
   return (
     <main className=" bg-gray-4 w-full h-screen bg-bgForm flex flex-col justify-between items-center">
